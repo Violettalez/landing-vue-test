b31aff6ef161e28926e36cc6821a4d3517f20e46
Violetta Lezhnenko <leznenkovioletta@gmail.com>
2025-10-16 12:49:18 +0300

    Refactor ContactUs component layout for improved responsiveness and typography

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -78,24 +78,29 @@ const validateForm = () => {
 }
 </script>
 <template>
-  <div class="bg-violet-bg/10 flex items-center p-30 gap-33">
-    <div class="w-[34%]">
-      <h2 class="font-museo font-medium text-[56px] leading-[66px] text-gray mb-8">Contact us!</h2>
-      <p class="font-montserrat text-lg text-gray-text mb-12">
-        The support staff and customer service are available to help you with any questions or
-        needs.
+  <div
+    class="bg-violet-bg/10 flex justify-between desktop:flex-row tablet:flex-col mobile:flex-col items-center desktop:p-30 tablet:py-12 tablet:px-[130px] mobile:py-12 mobile:px-5 desktop:gap-[132px] tablet:gap-12 mobile:gap-12"
+  >
+    <div class="desktop:w-[34%] tablet:w-full mobile:w-full">
+      <h2 class="font-museo font-bold text-[56px] leading-[66px] desktop:text-start tablet:text-center mobile:text-start text-gray mb-8">Contact us!</h2>
+      <p class="font-montserrat text-lg text-gray-text desktop:mb-12 tablet:mb-8 leading-[30px] mobile:mb-6">
+        The support staff and customer service are available to help you with any questions or needs
+        you might have. Just drop us a line.
       </p>
-      <div class="flex flex-col gap-[16px]">
+      <p class="font-montserrat text-base text-gray-text mb-6 leading-[27px]">This site is owned and operated by Kode Tech Solutions LTD</p>
+      <div class="flex flex-col gap-4">
         <div v-for="contact in contactInfo" :key="contact.id" class="flex flex-col gap-2">
-          <p class="font-museo font-medium text-[15px] text-gray">
-            <strong>{{ contact.type }}</strong>
+          <p class="font-museo font-bold text-[15px] text-gray">
+            {{ contact.type }}
           </p>
           <p class="font-montserrat text-base text-gray-text">{{ contact.value }}</p>
         </div>
       </div>
     </div>
 
-    <div class="bg-white border-2 border-violet rounded-[30px] p-20 flex-grow">
+    <div
+      class="bg-white border-2 border-violet rounded-[30px] p-20 desktop:w-[585px] tablet:w-full mobile:w-full tablet:py-20 tablet:px-[41px] mobile:py-12 mobile:px-4"
+    >
       <form @submit.prevent="validateForm" class="flex flex-col items-center gap-6 px-4 py-[7px]">
         <div class="w-full">
           <input
@@ -138,7 +143,7 @@ const validateForm = () => {
           </p>
         </div>
 
-        <button class="button">Send</button>
+        <button class="button w-auto">Send</button>
       </form>
     </div>
   </div>
